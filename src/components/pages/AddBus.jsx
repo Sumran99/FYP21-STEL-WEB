296a48c24463b765c79a5bfbccc01437d9305f55
KhurramRana <86261887+KhurramRana@users.noreply.github.com>
2022-05-06 17:27:43 +0500

    States

--- src/components/pages/AddBus.jsx
+++ src/components/pages/AddBus.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import "./addbus.css"
+import React, { useState } from "react";
+import "./addbus.css";
 
 export default function AddBus() {
-   
+  const [busnumber, setBusnumber] = useState("");
+  const [capacity, setCapacity] = useState("");
+  const [mileadge, setMileadge] = useState("");
+  const [chasenumber, setChasenumber] = useState("");
 
-    return (
-    <div  className='BUSconatiner'>
-      <h1>Khurram Rana</h1>
-      <div className='Textarea'>
-      <textarea></textarea><br/>
-      <button>Sumbit</button>
+
+  return (
+    <div className="BUSconatiner">
+      <h1 className="heading">Add Bus</h1>
+      <div className="Textarea">
+        <input
+          className="textarea"
+          placeholder="Bus Number"
+          type={Text}
+          value={busnumber}
+          onChange={(e) => {
+            setBusnumber(e.target.value);
+          }}
+        ></input>
+        <input
+          className="textarea"
+          placeholder="Students Capacity"
+          type={Text}
+          value={capacity}
+          onChange={(e) => {
+            setCapacity(e.target.value);
+          }}
+        ></input>
+        <input className="textarea" placeholder="Mileage" type={Text}
+        value={mileadge}
+        onChange={(e) => {
+          setMileadge(e.target.value);
+        }}></input>
+        <input
+          className="textarea"
+          placeholder="Chase Number"
+          type={Text}
+          value={chasenumber}
+          onChange={(e) => {
+            setChasenumber(e.target.value);
+          }}
+        ></input>
+        <button className="Button">Add</button>
       </div>
-
     </div>
-  )
+  );
 }
