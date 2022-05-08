1e479ff3ebc83528703cecf71700aba198304702
KhurramRana <86261887+KhurramRana@users.noreply.github.com>
2022-05-09 01:47:23 +0500

    AddRoute

--- src/components/pages/AddRoute.jsx
+++ src/components/pages/AddRoute.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React , {useState} from 'react'
+import "./addroute.css"
 
 export default function AddRoute() {
+  const [routenumber, setroutenumber] = useState("");
+  const [to, setto] = useState("");
+  const [from, setfrom] = useState("");
+
   return (
-    <div className='BUSconatiner'>AddRoute</div>
+    <div className='BUSconatiner'>
+    
+    <h1 className="heading">Add Route</h1>
+      <div class="solid" />
+      <div className="Textarea">
+        <input
+          className="textarea"
+          placeholder="Add Route Number"
+          type={Text}
+          value={routenumber}
+          onChange={(e) => {
+            setroutenumber(e.target.value);
+          }}
+        ></input>
+        <input
+          className="textarea"
+          placeholder=" To"
+          type={Text}
+          value={to}
+          onChange={(e) => {
+            setto(e.target.value);
+          }}
+        ></input>
+        <input
+          className="textarea"
+          placeholder="From"
+          type={Text}
+          value={from}
+          onChange={(e) => {
+            setfrom(e.target.value);
+          }}
+        ></input>
+        
+        <button className="Button">Add</button>
+      </div>
+    
+    </div>
   )
 }
