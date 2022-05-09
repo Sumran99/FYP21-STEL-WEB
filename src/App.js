15dab4407b726282fbb226651aa6c6f03068eecb
KhurramRana <86261887+KhurramRana@users.noreply.github.com>
2022-05-09 11:35:24 +0500

    New Modules added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import RemoveDriver from "./components/pages/RemoveDriver";
 import AddBus from "./components/pages/AddBus";
 import AddRoute from "./components/pages/AddRoute";
 import AddTRip from "./components/pages/AddTRip";
+import Removebus from "./components/pages/Removebus";
+import Removeroute from "./components/pages/Removeroute";
 
 function App() {
   return (
@@ -22,9 +24,10 @@ function App() {
           <Route path="/updatedriver" element={<UpdateDriver />} />
           <Route path="/removedriver" element={<RemoveDriver />} />
           <Route path="/addbus" element={<AddBus />} />
-          <Route path="/addroute"element={<AddRoute/>}/>
-          <Route path="/addtrip"element={<AddTRip/>}/>
-
+          <Route path="/addroute" element={<AddRoute />} />
+          <Route path="/addtrip" element={<AddTRip />} />
+          <Route path="/removebus" element={<Removebus />} />
+          <Route path="/removeroute" element={<Removeroute />} />
         </Routes>
       </div>
     </Router>
