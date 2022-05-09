879e2caef81984817873a0f2109924dba000ed81
KhurramRana <86261887+KhurramRana@users.noreply.github.com>
2022-05-09 11:44:33 +0500

    Side Text to white

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,19 +11,28 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sideTitle">Driver</h3>
           <ul className="sidebarList">
-            <Link style={{ textDecoration: 'none', color: 'grey' }}to="/adddriver">
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/adddriver"
+            >
               <li className="sidebarListItem active">
                 <AddIcon className="sidebaricon" />
                 Add Driver
               </li>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'white' }}to="/removedriver">
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/removedriver"
+            >
               <li className="sidebarListItem">
                 <DeleteIcon className="sidebaricon" />
                 Remove Driver
               </li>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'white' }}to="/updatedriver">
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/updatedriver"
+            >
               <li className="sidebarListItem">
                 <UpdateIcon className="sidebaricon" />
                 Update Driver
@@ -34,17 +43,23 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sideTitle">Bus</h3>
           <ul className="sidebarList">
-          <Link style={{ textDecoration: 'none', color: 'grey' }}to="/addBus">
-            <li className="sidebarListItem">
-              <AddIcon className="sidebaricon" />
-              Add Bus
-            </li>
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/addBus"
+            >
+              <li className="sidebarListItem">
+                <AddIcon className="sidebaricon" />
+                Add Bus
+              </li>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'grey' }}to="/removebus">
-            <li className="sidebarListItem">
-              <DeleteIcon className="sidebaricon" />
-              Remove Bus
-            </li>
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/removebus"
+            >
+              <li className="sidebarListItem">
+                <DeleteIcon className="sidebaricon" />
+                Remove Bus
+              </li>
             </Link>
             <li className="sidebarListItem">
               <UpdateIcon className="sidebaricon" />
@@ -55,17 +70,23 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sideTitle">Route</h3>
           <ul className="sidebarList">
-          <Link style={{ textDecoration: 'none', color: 'grey' }}to="/addroute">
-            <li className="sidebarListItem active">
-              <AddIcon className="sidebaricon" />
-              Add Route
-            </li>
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/addroute"
+            >
+              <li className="sidebarListItem active">
+                <AddIcon className="sidebaricon" />
+                Add Route
+              </li>
             </Link>
-            <Link style={{ textDecoration: 'none', color: 'grey' }}to="/removeroute">
-            <li className="sidebarListItem">
-              <DeleteIcon className="sidebaricon" />
-              Remove Route
-            </li>
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/removeroute"
+            >
+              <li className="sidebarListItem">
+                <DeleteIcon className="sidebaricon" />
+                Remove Route
+              </li>
             </Link>
             <li className="sidebarListItem">
               <UpdateIcon className="sidebaricon" />
@@ -76,11 +97,14 @@ export default function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sideTitle">Trip</h3>
           <ul className="sidebarList">
-          <Link style={{ textDecoration: 'none', color: 'grey' }}to="/addtrip">
-            <li className="sidebarListItem active">
-              <AddIcon className="sidebaricon" />
-              Add Trip
-            </li>
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/addtrip"
+            >
+              <li className="sidebarListItem active">
+                <AddIcon className="sidebaricon" />
+                Add Trip
+              </li>
             </Link>
             <li className="sidebarListItem">
               <DeleteIcon className="sidebaricon" />
@@ -92,7 +116,7 @@ export default function Sidebar() {
             </li>
           </ul>
         </div>
-        
+
         <div className="sidebarMenu">
           <h3 className="sideTitle">Reports</h3>
           <ul className="sidebarList">
