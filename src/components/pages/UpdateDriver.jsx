8b4628a2f831d7c34e52d56339a2b0a9f24b0316
Raja Faizan Nazir <RFaizanN@gmail.com>
2022-05-16 22:57:58 +0500

    view data

--- src/components/pages/UpdateDriver.jsx
+++ src/components/pages/UpdateDriver.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
-import "./updatedriver.css"
+import React from "react";
+import "./updatedriver.css";
 export default function UpdateDriver() {
+  const data = fetch("/api/driver/drivers", {
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((resp) => console.log(resp))
+    .catch((err) => console.log(err));
   return (
-    <div className='boxes'>
-
-      {/* <input className='textarea' placeholder='Enter Driver CNIC'></input>
-      <button className='Buttonupdate'>Serach Driver</button>
-       */}
-       <form action="" class="search-bar">
-	<input type="search" name="search" pattern=".*\S.*" required/>
-	<button class="search-btn" type="submit">
-		<span>Search</span>
-	</button>
-</form>
+    <div className="boxes">
+      Hello world: {data}
+      <input className="textarea" placeholder="Enter Driver CNIC"></input>
+      <button className="Buttonupdate">Serach Driver</button>
+      <form action="" class="search-bar">
+        <input type="search" name="search" pattern=".*\S.*" required />
+        <button class="search-btn" type="submit">
+          <span>Search</span>
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
+/****************************** */
+/*
+const data = fetch("/api/driver/drivers", {
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+    // (var) payload looks like this {Header: "Sending", . . .}
+  })
+    .then((res) => res.json())
+    .then((resp) => console.log(resp))
+    .catch((err) => console.log(err));
+    */
