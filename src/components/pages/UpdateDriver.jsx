30d56d3cce545f8453e56b6709e53de0dcf13379
Raja Faizan Nazir <RFaizanN@gmail.com>
2022-05-17 20:41:49 +0500

    Update UpdateDriver.jsx

--- src/components/pages/UpdateDriver.jsx
+++ src/components/pages/UpdateDriver.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./updatedriver.css";
+import axios from "axios";
+import { useState } from "react";
+
 export default function UpdateDriver() {
-  const data = fetch("/api/driver/drivers", {
-    method: "get",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((resp) => console.log(resp))
-    .catch((err) => console.log(err));
+  // const router = useRouter();
+  // const { id } = router.query;
+  // console.log("slug", id);
+  const [blogdata, setBlogData] = useState({});
+  const [loader, setLoader] = useState(false);
+  // const styleObj = {
+  //   color: "black",
+  // };
+
+  useEffect(() => {
+    //setLoader(true);
+    // console.log("hello from useEffect");
+    axios
+      .get(`/api/driver/drivers`)
+      .then((response) => {
+        console.log("response.data", response.data);
+        setBlogData(response.data.data);
+      })
+      .catch((error) => {
+        //setLoader(false);
+      });
+  }, []);
   return (
     <div className="boxes">
-      Hello world: {data}
+      Hello world: {setBlogData}
       <input className="textarea" placeholder="Enter Driver CNIC"></input>
       <button className="Buttonupdate">Serach Driver</button>
       <form action="" class="search-bar">
@@ -28,6 +43,7 @@ export default function UpdateDriver() {
 
 /****************************** */
 /*
+//   "/api/driver/drivers"
 const data = fetch("/api/driver/drivers", {
     method: "get",
     headers: {
