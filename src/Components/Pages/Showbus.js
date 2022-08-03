231a24cc3cb36524a26c5e4af6e818bf9cf9b936
Khurram Rana <rgee788@gmail.com>
2022-08-04 02:54:33 +0500

    view buses

--- src/Components/Pages/Showbus.js
+++ src/Components/Pages/Showbus.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "busnumber", headerName: "Bus No", width: 120 },
+  { field: "chasenumber", headerName: "Chase No", width: 120 },
+  { field: "millage", headerName: "Mileage", width: 120 },
+  { field: "capacity", headerName: "Capacity", width: 120 },
+  { field: "ac", headerName: "AC", width: 70 },
+  { field: "active", headerName: "Active", width: 70 },
+];
 
 const Showbus = () => {
-  return (
-    <div>Showbus</div>
-  )
-}
+  const [buses, setBuses] = React.useState([]);
 
-export default Showbus
+  const getAllBuses = async () => {
+    const response = await fetch("/api/bus/buses");
+    const data = await response.json();
+    setBuses(data.Buses);
+    console.log(buses);
+  };
+
+  React.useEffect(() => {
+    getAllBuses();
+  }, [buses]);
+  return (
+    <>
+      <Grid item xs={12}>
+        <h1>Buses Details</h1>
+      </Grid>
+      <Grid item xs={12}>
+        <Box sx={{ height: 400, width: "50%" }}>
+          <DataGrid
+            rows={buses}
+            columns={columns}
+            getRowId={(row) => row._id}
+          />
+        </Box>
+      </Grid>
+    </>
+  );
+};
+
+export default Showbus;
