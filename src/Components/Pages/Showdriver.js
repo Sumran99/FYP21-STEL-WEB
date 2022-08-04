9f6ec047e1a1f2d2c9a47ffbd373e7db34e12e37
Khurram Rana <rgee788@gmail.com>
2022-08-04 18:39:09 +0500

    Showdriver

--- src/Components/Pages/Showdriver.js
+++ src/Components/Pages/Showdriver.js
@@ -1,7 +1,46 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/system";
+import { DataGrid } from "@mui/x-data-grid";
+const columns = [
+  { field: "name", headerName: "Name", width: 120 },
+  { field: "phone", headerName: "Phone No", width: 120 },
+  { field: "cnic", headerName: "CNIC", width: 120 },
+  { field: "license", headerName: "License No", width: 120 },
+  { field: "experince", headerName: "Experince", width: 70 },
+];
 
 const Showdriver = () => {
-  return <div>Showdriver</div>;
+  const [driver, setdriver] = React.useState([]);
+
+  const getAllDrivers = async () => {
+    const response = await fetch(
+      "https://stel-api.herokuapp.com/api/driver/drivers"
+    );
+    const data = await response.json();
+    setDriver(data.Buses);
+    console.log(driver);
+  };
+
+  React.useEffect(() => {
+    getAllDrivers();
+  }, [driver]);
+  return (
+    <>
+      <Grid item xs={12}>
+        <h1>Driver Details</h1>
+      </Grid>
+      <Grid item xs={12}>
+        <Box sx={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={driver}
+            columns={columns}
+            getRowId={(row) => row._id}
+          />
+        </Box>
+      </Grid>
+    </>
+  );
 };
 
 export default Showdriver;
